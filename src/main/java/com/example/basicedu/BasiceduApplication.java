e9a853e49a0ca8f0954d548f45d6d7592de7cad4
gek2omg <gek2omg@gmail.com>
2022-09-28 13:52:29 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/basicedu/BasiceduApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasiceduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasiceduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicedu/BasiceduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasiceduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
